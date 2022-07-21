05689f3be97d43bea6ba30d7fc8a416b79398f89
Anderson (Akame!) <andersonrobertopetry@gmail.com>
2022-07-21 14:59:36 -0300

    ajust

--- js/main.js
+++ js/main.js
@@ -51,9 +51,9 @@ controle.forEach( (elemento)  => {
 function manipulaDados(operacao, controle){
     const peca = controle.querySelector(".controle-contador");
     if(operacao === "-") {
-        peca.value = parseInt(peca.value) + 1;
-    } else{
         peca.value = parseInt(peca.value) - 1;
+    } else{
+        peca.value = parseInt(peca.value) + 1;
     }
 }
 
